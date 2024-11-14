e944dbc8a6a6b042ab0202edf2c487026e2aac76
sidram403 <siddharthpatil9108@gmail.com>
2024-11-14 23:34:04 +0530

    Added input component as client side and update the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Input from '../components/Input'
 
 const page = () => {
   return (
-    <div>
-      Home
+    <div className='max-w-xl mx-auto border-r border-l min-h-screen'>
+      <div className='py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+        <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
+      </div>
+      <Input />
     </div>
   )
 }
