7f13e557018c7c77a2785e51feb55a2fd095d3a6
sidram403 <siddharthpatil9108@gmail.com>
2024-11-14 23:08:56 +0530

    added mini profile to the sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,13 +3,15 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 import { FaXTwitter } from "react-icons/fa6";
-import { HiHome } from "react-icons/hi";
+import { HiHome, HiDotsHorizontal } from "react-icons/hi";
 
 const Sidebar = () => {
   const { data: session } = useSession();
-
+    console.log(session);
+    
   return (
-    <div className="flex flex-col gap-4 p-3">
+    <div className="flex flex-col p-3 justify-between h-screen">
+        <div className="flex flex-col gap-4 p-3">
       <Link href={"/"}>
         <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
       </Link>
@@ -34,6 +36,17 @@ const Sidebar = () => {
         >
           Sign In
         </button>
+      )}
+      </div>
+      {session && (
+        <div className="text-gray-700 text-sm flex items-center cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200">
+            <img src={session.user.image} alt="user-img" className="h-10 w-10 rounded-full xl:mr-2" />
+            <div className="hidden xl:inline">
+                <h4 className="font-bold">{session.user.name}</h4>
+                <h4 className="text-gray-500">{session.user.username}</h4>
+            </div>
+            <HiDotsHorizontal className="xl:ml-8 hidden xl:inline h-5" />
+        </div>
       )}
     </div>
   );
